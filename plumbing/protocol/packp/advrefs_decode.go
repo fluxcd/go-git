fluxcd/go-git

commit 28b82b9b44060722f8fe31eb1e25fe6f2e1e1234

fix memory leak in packp.readRef()
Copy slice contents into variables instead of returning the element by
referring to the index. This allows for the slice to be cleaned up by
the GC.

Signed-off-by: Sanskar Jaiswal <jaiswalsanskar078@gmail.com>

plumbing/protocol/packp/advrefs_decode.go
@@ -248,7 +248,10 @@ func readRef(data []byte) (string, plumbing.Hash, error) {
 	case len(chunks) > 2:
 		return "", plumbing.ZeroHash, fmt.Errorf("malformed ref data: more than one space found")
 	default:
-		return string(chunks[1]), plumbing.NewHash(string(chunks[0])), nil
+		var ref, hash []byte
+		copy(ref, chunks[1])
+		copy(hash, chunks[0])
+		return string(ref), plumbing.NewHash(string(hash)), nil
 	}
 }
 

commit 714664dbfaabe4b00a2d4403681dcad66d396414

initialize slices in packp.readRefs() with a fixed size
Fix regression introduced in 28b82b9, which tried to fix a memory leak
by copying slice elements into standalone vars, but doesn't work since
copy() only copies until the array's length.

Signed-off-by: Sanskar Jaiswal <jaiswalsanskar078@gmail.com>

plumbing/protocol/packp/advrefs_decode.go
@@ -248,7 +248,8 @@ func readRef(data []byte) (string, plumbing.Hash, error) {
 	case len(chunks) > 2:
 		return "", plumbing.ZeroHash, fmt.Errorf("malformed ref data: more than one space found")
 	default:
-		var ref, hash []byte
+		ref := make([]byte, len(chunks[1]))
+		hash := make([]byte, len(chunks[0]))
 		copy(ref, chunks[1])
 		copy(hash, chunks[0])
 		return string(ref), plumbing.NewHash(string(hash)), nil